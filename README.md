49ab4c2a0c4a82a7edca19518c9c65518edaaf19
dkurchigin <dkurchigin@t-argos.ru>
2021-06-18 11:38:26 +0300

    add all for server

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # er_telecom
+
+1) Need Redis for the server
+2) Need Celery. Run as:
+	celery -A server.celery worker --loglevel=info --concurrency=4 -P gevent
+	gevent - for Windows only
--- server.py
+++ server.py
@@ -3,72 +3,59 @@ import json
 import numpy
 import time
 import hashlib
-# from celery import Celery
-#
-# broker_url = 'amqp://guest@localhost'          # Broker URL for RabbitMQ task queue
+from celery import Celery
+import redis
 
+broker_url = 'redis://localhost:6379/0'
 app = Flask(__name__)
+r = redis.Redis()
 
+celery = Celery(app.name, broker=broker_url)
+app.config.from_object("config")
 
+@celery.task
 def sort_array(array_, hash_, sort_reverse=False):
+    r.get(hash_)
     cleared_array = [element for element in array_ if isinstance(element, int) or isinstance(element, float)]
     cleared_array.sort(reverse=sort_reverse)
     cleared_array.append(round(numpy.average(cleared_array), 2))
-    time.sleep(5)
-    tasks[hash_] = cleared_array
+    # time.sleep(20)
+    r.set(hash_, json.dumps(cleared_array))
 
-tasks = {}
-
-# celery = Celery(app.name, broker=broker_url)
-# celery.config_from_object('celeryconfig')      # Your celery configurations in a celeryconfig.py
-
-# @celery.task(bind=True)
-# def some_long_task(self, x, y):
-#     # Do some long task
-#     ...
+def calc_hash(addr):
+    current_timestamp = time.time()
+    return hashlib.md5(f'{current_timestamp}{addr}'.encode()).hexdigest()
 
 @app.route('/api/sorting', methods=['POST'])
 def get_sort():
     if request.method == 'POST':
         data = request.get_json()
-        if data.get('token'):
-            token = data['token']
-            if token in tasks.keys():
-                if tasks[token] != False:
-                    return Response(
-                        json.dumps({'array': tasks[token]}),
-                        content_type='application/json',
-                        status=200
-                    )
-                else:
-                    return Response(
-                        json.dumps({'array': 'in progress'}),
-                        content_type='application/json',
-                        status=200
-                    )
-            else:
-                return Response(
-                    status=400,
-                    response=f'Can\'t find token {token} in DB'
-                )
-        elif data.get('array'):
-            user_ip = request.remote_addr
-            current_timestamp = time.time()
-            hashed_ts = hashlib.md5(f'{current_timestamp}{user_ip}'.encode()).hexdigest()
-            tasks[hashed_ts] = False
-            sort_array(data['array'], hashed_ts)
-            return Response(
-                json.dumps({'token': hashed_ts}),
-                content_type='application/json',
-                status=200
-            )
-        else:
-            return Response(
-                status=400,
-                response = 'Response without \'array\' or \'token\'in JSON. Check right response structure'
-            )
 
+        if data.get('token'):
+            # CHECK OUR ARRAY BY TOKEN
+            token = data['token']
+            if r.exists(token):
+                entry = r.get(token)
+                if entry != b'':
+                    entry = json.loads(entry)
+                    return Response(json.dumps({'array': entry}), content_type='application/json', status=200)
+                else:
+                    return Response(json.dumps({'array': 'in progress'}), content_type='application/json', status=200)
+            else:
+                return Response(status=400, response=f'Can\'t find token {token} in DB')
+
+        elif data.get('array'):
+            # TRY TO SORT ARRAY
+            hashed_ts = calc_hash(request.remote_addr)
+            r.set(hashed_ts, '')
+            sorting_type = data.get('sort_reverse') if isinstance(data.get('sort_reverse'), bool) else False
+            sort_array.delay(data['array'], hashed_ts, sorting_type)
+            return Response(json.dumps({'token': hashed_ts}), content_type='application/json', status=200)
+
+        else:
+            return Response(status=400, response = 'Response without \'array\' or \'token\'in JSON. Check right response structure')
 
 if __name__ == '__main__':
+    r.flushall()
     app.run()
 
